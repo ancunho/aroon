f3c770e700985fc7a172fd3ec93b565d995f6ebe
cunho <ancunho@naver.com>
2026-02-14 23:13:36 +0900

    asdf

--- src/main/java/com/aroon/business/controller/UserController.java
+++ src/main/java/com/aroon/business/controller/UserController.java
@@ -1,13 +1,13 @@
 package com.aroon.business.controller;
 
 import com.aroon.business.common.response.ApiResponse;
+import com.aroon.business.dto.request.UserCreateRequest;
 import com.aroon.business.dto.response.UserResponse;
+import com.aroon.business.entity.core.User;
 import com.aroon.business.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +21,16 @@ public class UserController {
     @GetMapping
     public ApiResponse<List<UserResponse>> getAllUsers() {
         return ApiResponse.success(userService.getAllUsers());
+    }
+
+    @GetMapping("/{userSeq}")
+    public ApiResponse<UserResponse> getUserBySeq(@PathVariable Long userSeq) {
+        return ApiResponse.success(userService.getUserBySeq(userSeq));
+    }
+
+    @PostMapping
+    public ApiResponse<UserResponse> createUser(@RequestBody UserCreateRequest userCreateRequest) {
+        return ApiResponse.success(userService.createUser(userCreateRequest));
     }
 
 }
